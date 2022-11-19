e3b744ee7c7b113ac845505d9c45b8f20630b120
Marcelo Giati <mtgiati@gmail.com>
2022-11-19 13:56:48 -0300

    linha02

--- src/js/anime.js
+++ src/js/anime.js
@@ -4,3 +4,8 @@
 function imgSlider(anything){
   document.querySelector('.starbucks').src = anything;
 }
+
+function changeCircleColor(color){
+  const circle = document.querySelector('.circle');
+  circle.style.background = color;
+}
